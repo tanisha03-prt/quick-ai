6a240c14d590d7b9e7116a09f4dc67ae9f206a4c
Tanisha <tanishasingh0302@gmail.com>
2026-07-15 15:49:30 +0530

    User Authentication Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import Community from "./pages/Community";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-export default App
+      <Route path="/ai" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="write-article" element={<WriteArticle />} />
+        <Route path="blog-titles" element={<BlogTitles />} />
+        <Route path="community" element={<Community />} />
+        <Route path="generate-images" element={<GenerateImages />} />
+        <Route path="remove-background" element={<RemoveBackground />} />
+        <Route path="remove-object" element={<RemoveObject />} />
+        <Route path="review-resume" element={<ReviewResume />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
       <h1>Layout</h1>
-    </div>
-  )
-}
 
-export default Layout
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
