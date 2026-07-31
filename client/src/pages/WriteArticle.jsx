21a611f12ac8d3f853dadeef80ca9120de10aa19
Tanisha <tanishasingh0302@gmail.com>
2026-07-31 20:29:21 +0530

    Added Article Configuration in WriteArticle

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Sparkles } from "lucide-react";
 
 const WriteArticle = () => {
   return (
-    <div>
-      <h1>WriteArticle</h1>
-    </div>
-  )
-}
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
 
-export default WriteArticle
+      {/* Left Column */}
+
+      <form className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">
+            Article Configuration
+          </h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">
+          Article Topic
+        </p>
+
+        <input
+          type="text"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="The future of artificial intelligence is..."
+          required
+        />
+
+        <p className="mt-4 text-sm font-medium">
+          Article Length
+        </p>
+
+      </form>
+
+      {/* Right Column */}
+
+      <div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default WriteArticle;
